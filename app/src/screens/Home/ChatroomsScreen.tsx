de5e627f1beeaa7329e16768a6fed84b863e71b9
iori0213 <iori19980213@gmail.com>
2022-03-23 04:09:27 +0800

    Anji's Cli socket demonstration

--- app/src/screens/Home/ChatroomsScreen.tsx
+++ app/src/screens/Home/ChatroomsScreen.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { socket } from '../../../App';
 import { bg_DarkColor } from '../../constants/cssConst';
 import { ChatroomsScreenProps } from '../../types/screenProps';
 
@@ -9,10 +10,48 @@ const ChatroomsScreen: React.FC<ChatroomsScreenProps> = ({ }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <Text style={{ color: "#FFF" }}>Chatrooms</Text>
+      <TestComp />
     </SafeAreaView>
   );
 }
 export default ChatroomsScreen;
+/**
+ * socket.on('some-event', (param) => {
+      param === {
+        email: 'email',
+        password: 'password'
+      }
+
+      param.email, param.passord
+ *                           
+ * })
+ */
+
+const TestComp: React.FC = () => {
+  useEffect(() => {
+    socket.on('server-emitted-event', (params) => {
+      // do something
+    })
+
+    return () => {
+      socket.off('server-emitted-event')
+    }
+  })
+
+  const testHandler = () => {
+    socket.emit('some-event', {
+      email: 'email',
+      password: 'password',
+
+    })
+  }
+
+  return (
+    <View>
+      <Button title='test' onPress={testHandler} />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   safeArea: {
