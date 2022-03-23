663da21fd220b98bec31d1dbfe52b286ab7d17d5
iori0213 <iori19980213@gmail.com>
2022-03-23 18:02:12 +0800

    add get friendship request, but friendship only contain friendship.id

--- server/src/routes/friendShip.ts
+++ server/src/routes/friendShip.ts
@@ -6,9 +6,8 @@ import { Profile } from "../entity/Profile";
 import { TokenAuthentication } from "../middlewares/TokenAuthentication.middleware";
 
 const router = express();
-
+//SECTION Add friend
 router.post("/add", TokenAuthentication, async (req, res) => {
-  console.log('Adding Friend')
   //check id input
   const userid = req.profile.id;
   const { friendid } = req.body;
@@ -20,25 +19,53 @@ router.post("/add", TokenAuthentication, async (req, res) => {
   const friendShipRepo = getRepository(FriendShip);
 
   //get user profile
-  const userProfile = await profileRepo.findOne({ where: { id: userid } })
+  const userProfile = await profileRepo.findOne({ where: { id: userid }, relations: ["friendShips"] })
   if (!userProfile) { return res.json({ success: false, message: "User profile not exist", line: 20 }) }
   //check if friend rpofile exist
-  const friendProfile = await profileRepo.findOne({ where: { id: friendid } })
+  const friendProfile = await profileRepo.findOne({ where: { id: friendid }, relations: ["friendShips"] })
   if (!friendProfile) { return res.json({ success: false, message: "friend profile not exist", line: 23 }) }
 
+  const friendshipCheck = await friendShipRepo.findOne({ where: { user: userProfile, friend: friendProfile } })
+  if (friendshipCheck) { return res.json({ success: false, message: "already is friend" }) }
+  //user
   const userFriendShip = new FriendShip()
   userFriendShip.friend = friendProfile
   userFriendShip.user = userProfile
   friendShipRepo.save(userFriendShip)
-
-
+  userProfile.friendShips.push(userFriendShip)
+  console.log("USER:", userProfile.friendShips);
+  profileRepo.save(userProfile)
+  //friend
   const friendFriendShip = new FriendShip()
   friendFriendShip.friend = userProfile
   friendFriendShip.user = friendProfile
-
   friendShipRepo.save(friendFriendShip)
+  friendProfile.friendShips.push(friendFriendShip);
+  profileRepo.save(friendProfile)
 
 
   return res.json({ success: true, message: "Add friend success.", line: 32 })
 })
+//!SECTION 
+
+//SECTION Get friends
+router.post("/get", TokenAuthentication, async (req, res) => {
+  //test
+  console.log(req.body.test);
+  const userid = req.profile.id;
+  const profileRepo = getRepository(Profile);
+  const userProfile = await profileRepo.findOne({ where: { id: userid }, relations: ["friendShips"] })
+  if (!userProfile) { return res.json({ success: false, message: "user not exist", line: 68 }) }
+  // BAD Solution
+  // //  const friendShipRepo = getRepository(FriendShip);
+  // // let FriendList:Profile[];
+  // // userProfile.friendShips.map(async(friendlist)=>{
+  // //   const list = await friendShipRepo.findOne({where:{id:friendlist.id}})
+  // //   FriendList.push(list!.friend)
+  // // })
+  console.log("ALL DATA:", userProfile);
+  userProfile.friendShips.map((friend_ship) => { console.log("Friend id :", friend_ship) })
+  return res.json({ success: true, message: "Get friendship success.", profile: userProfile })
+})
+//!SECTION 
 export { router as friendShipRouter }
