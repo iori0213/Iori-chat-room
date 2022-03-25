2ccebfa3f03129dcefae4698a324a1528dcacf9a
iori0213 <iori19980213@gmail.com>
2022-03-25 16:39:22 +0800

    update backend api constant

--- app/src/constants/backendAPI.ts
+++ app/src/constants/backendAPI.ts
@@ -1,3 +1,5 @@
 export const BackendUrl = `http://192.168.0.2:5000`;
 export const AuthAPI = `${BackendUrl}/api/v1/auth`;
 export const UserAPI = `${BackendUrl}/api/v1/user`;
+export const FriendAPI = `${BackendUrl}/api/v1/friendship`
+export const ChatRoomAPI = `${BackendUrl}/api/v1/chatroom`
