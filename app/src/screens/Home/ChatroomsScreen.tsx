e42b47a17954ad91a3703f0a2257af3d7de25a12
iori0213 <iori19980213@gmail.com>
2022-03-25 23:11:25 +0800

    basic layout and useState of chat room

--- app/src/screens/Home/ChatroomsScreen.tsx
+++ app/src/screens/Home/ChatroomsScreen.tsx
@@ -1,15 +1,53 @@
-import React, { useEffect } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { socket } from '../../../App';
-import { bg_DarkColor } from '../../constants/cssConst';
+import { bg_DarkColor, bg_LessDarkColor, windowHeight, windowWidth } from '../../constants/cssConst';
 import { ChatroomsScreenProps } from '../../types/screenProps';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const ChatroomsScreen: React.FC<ChatroomsScreenProps> = ({ }) => {
+  const [chatName, setChatName] = useState<string>("");
+  const [roomList, setRoomList] = useState<RoomList[]>([]);
+  const [showingList, setShowingList] = useState<RoomList[]>([]);
+  const getChatRoom = async () => {
+    axios({
+      method: "get"
+    })
+  }
+  const createChatRoom = async () => {
+    axios({
+      method: "get",
+
+    })
+  }
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Text style={{ color: "#FFF" }}>Chatrooms</Text>
+      <View style={styles.header}>
+        <View style={styles.titleBar}>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Chat Rooms</Text>
+          </View>
+          <TouchableOpacity style={styles.addBtn} onPress={() => createChatRoom()}>
+            <MaterialCommunityIcons name="chat-plus" size={windowWidth * 0.1} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.searchBar}>
+          <TextInput
+            onChangeText={(val) => setChatName(val)}
+            placeholder="Search room name"
+            placeholderTextColor="#999"
+            autoCapitalize="none"
+            style={styles.searchInput}
+          />
+          <TouchableOpacity style={styles.goBtn} onPress={() => console.log("search room")}>
+            <Ionicons name="search" size={windowWidth * 0.07} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+      </View>
       <TestComp />
     </SafeAreaView>
   );
@@ -57,7 +95,56 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: bg_DarkColor,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    width: windowWidth,
+    height: windowHeight * 0.12,
+    backgroundColor: bg_LessDarkColor,
+
+  },
+  titleBar: {
+    height: windowHeight * 0.06,
+    width: windowWidth,
+    flexDirection: "row",
+  },
+  title: {
+    flex: 9,
+  },
+  titleText: {
+    flex: 1,
+    textAlignVertical: "center",
+    fontSize: windowWidth * 0.08,
+    color: "#FFF",
+    paddingLeft: windowWidth * 0.02,
+  },
+  addBtn: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingRight: windowWidth * 0.03,
+  },
+  searchBar: {
+    height: windowHeight * 0.04,
+    width: windowWidth,
+    paddingHorizontal: windowWidth * 0.02,
+    flexDirection: "row",
+    marginVertical: windowHeight * 0.01,
+  },
+  searchInput: {
+    flex: 9,
+    borderRadius: windowHeight * 0.02,
+    paddingLeft: windowWidth * 0.04,
+    backgroundColor: bg_DarkColor,
+    color: "#FFF",
+  },
+  goBtn: {
+    flex: 1,
+    paddingLeft: windowWidth * 0.01,
   }
 })
+
+function async() {
+  throw new Error('Function not implemented.');
+}
--- app/src/types/types.ts
+++ app/src/types/types.ts
@@ -2,3 +2,8 @@ type Friend = {
   id: string;
   username: string;
 }
+
+type RoomList = {
+  id: string;
+  roomname: string;
+}
