6f3c99d7350bf55e3542125345ea16a9f3e48ff6
Anji <nhu.nhu.face@gmail.com>
2022-03-26 23:13:36 +0900

    socket folder structure and flow

--- server/src/socket/socketController.ts
+++ server/src/socket/socketController.ts
@@ -1,9 +1,28 @@
-import { Server, Socket } from 'socket.io'
+import { Server, Socket } from "socket.io";
+import { getRepository } from "typeorm";
+import { Profile } from "../entity/Profile";
+import { jwtDecode } from "../utils/jwtController";
+import { chatController } from "./chat";
 
-export const socketController = (_io: Server, socket: Socket) => {
-  socket.on("some-event", (one, two) => {
-    console.log("listen from some-event", {
-      one, two
+export const socketController = async (io: Server, socket: Socket) => {
+  const accessToken: string = socket.handshake.auth.token;
+  const decodedToken = jwtDecode<AccessToken>(accessToken);
+  if (!decodedToken) {
+    return socket.emit("error", {
+      message: "Token is not valid",
     });
-  });
+  } else {
+    const user = await getRepository(Profile).findOne({
+      where: { id: decodedToken.id },
+    });
+
+    if (!user) {
+      return socket.emit("error", {
+        message: "User not found",
+      });
+    }
+    console.log("connected user is: ", user.username, "...");
+    // FUNCTIONS
+    chatController(io, socket, user);
+  }
 };
