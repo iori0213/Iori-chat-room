3d9633209dd7d8197a6f3d9af96e820d6b4bc18e
iori0213 <iori19980213@gmail.com>
2022-04-02 23:51:01 +0800

    add get userInfo method

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,44 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 import express from "express";
 import { getRepository } from "typeorm";
 import User from "../entity/User";
-// import { TokenAuthentication } from "../middlewares/TokenAuthentication.middleware";
-import Jwt from "jsonwebtoken";
-
+import { TokenAuthentication } from "../middlewares/TokenAuthentication.middleware";
 
 const router = express.Router();
 
 //ANCHOR get Home page user info
-router.post("/userinfo", async (req, res) => {
+router.get("/userinfo", TokenAuthentication, async (req, res) => {
+  const userId = req.profile.id;
   const getRepo = getRepository(User);
-  const { accessToken } = req.body;
-  const userId = Jwt.decode(accessToken)
   const userInfo = await getRepo.findOne({
     select: [],
     where: { id: userId },
-    relations: ["friends"]
   });
-  if (!userInfo) return res.status(404).json({ success: false, message: "User not found!" })
+  if (!userInfo)
+    return res.status(404).json({ success: false, message: "User not found!" });
   console.log(userInfo);
-  return res.status(200).json({ success: true, userInfo: userInfo })
-})
+  return res.status(200).json({ success: true, userInfo: userInfo });
+});
 
-// ANCHOR get all user request
-// router.get("/userInfo", TokenAuthentication, async (req, res) => {
-//   const getRepo = getRepository(User);
-//   getRepo.find({
-//     select: ["id", "email", "name", "friends"],
-//     where: {
-//       id: req.user!.id
-//     },
-//     relations: ["FriendList"]
-//   })
-//     .then((data) => {
-//       return res.json(data);
-//     }).catch(error => console.log(error))
-// });
-
-
-
-
-export { router as userRouter }
+export { router as userRouter };
