96b8df23a60e4d1f6b3b43ce44fcc935b0edd08e
iori0213 <iori19980213@gmail.com>
2022-09-28 01:46:18 +0800

    make the token refresh by interceptor

--- app/src/routes/RootRouter.tsx
+++ app/src/routes/RootRouter.tsx
@@ -3,64 +3,47 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //import navigations
 import AuthNavigation from "../navigations/AuthNavigation";
-//import secureStore
-import * as SecureStore from "expo-secure-store";
-import axios from "axios";
 import { AuthAPI } from "../constants/backendAPI";
-import { ACCESS_KEY, REFRESH_KEY } from "../constants/securestoreKey";
 import LoadingSpinner from "../components/Auth/LoadingSpinner";
 import { RootNavigationProps } from "../types/navigations";
 import HomeStackNavigation from "../navigations/HomeStackNavigation";
+import customAxiosInstance from "../utils/customAxiosInstance";
 
 const RootRouter: React.FC = () => {
   const Stack = createNativeStackNavigator<RootNavigationProps>();
   //fetching data status
   const [fetching, setFetching] = useState<boolean>(true);
   // ANCHOR start token validation to do conditional navigation
-  const [access, setAccess] = useState<boolean>(false);
-  const [accessToken, setAccessToken] = useState<string>("");
-  const tokenValidation = async () => {
-    const localAccessToken = await SecureStore.getItemAsync(ACCESS_KEY);
-    console.log("accessToken:", localAccessToken);
-    const localRefreshToken = await SecureStore.getItemAsync(REFRESH_KEY);
-    if (!localAccessToken || !localRefreshToken) {
-      return false;
-    }
-    const accessResult = await axios({
+  const [access, setAccess] = useState<boolean>();
+  const tokenValidation = () => {
+    customAxiosInstance({
       method: "post",
       url: `${AuthAPI}/token/access`,
-      headers: { Authorization: `Bearer ${localAccessToken}` },
-    });
-    if (accessResult.data.success) {
-      setAccessToken(localAccessToken);
-      return true;
-    }
-    const refreshResult = await axios({
-      method: "post",
-      url: `${AuthAPI}/token/refresh`,
-      headers: { Authorization: `Bearer ${REFRESH_KEY}` },
-    });
-    if (refreshResult.status != 200) {
-      return false;
-    }
-    await SecureStore.setItemAsync(ACCESS_KEY, refreshResult.data.accessToken);
-    return true;
-  };
-
-  useEffect(() => {
-    try {
-      tokenValidation().then((isAccess) => {
-        if (isAccess) {
+    })
+      .then((res) => {
+        if (res.status === 200) {
           setAccess(true);
         } else {
           setAccess(false);
         }
-        setFetching(false);
+      })
+      .catch((e) => {
+        console.log(e.response.data.message);
+        setAccess(false);
       });
-    } catch (error) {
-      console.error(error);
+  };
+
+  useEffect(() => {
+    if (access === undefined) {
+      tokenValidation();
     }
   }, []);
+
+  useEffect(() => {
+    if (access !== undefined) {
+      setFetching(false);
+    }
+  }, [access]);
 
   return fetching ? (
     <LoadingSpinner />
